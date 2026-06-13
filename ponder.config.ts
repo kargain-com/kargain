4f2d9f5492ea32a0fa3c8022b14d1690951b631f
digirayc <kargain.org@gmail.com>
2026-06-13 13:47:36 +0300

    fix: startBlock 42781255, maxRequestsPerSecond 50

--- ponder.config.ts
+++ ponder.config.ts
@@ -19,7 +19,7 @@ export default createConfig({
       id: 84532,
       rpc:
         process.env.PONDER_RPC_URL_84532 ?? "https://sepolia.base.org",
-      maxRequestsPerSecond: 10,
+      maxRequestsPerSecond: 50,
     },
   },
   contracts: {
@@ -27,19 +27,19 @@ export default createConfig({
       chain: "baseSepolia",
       abi: KarPassportAbi,
       address: "0xe3568875db58be8e0ba6f44bf2a1178bb6777c29",
-      startBlock: 42781250,
+      startBlock: 42781255,
     },
     KarProPass: {
       chain: "baseSepolia",
       abi: KarProPassAbi,
       address: "0x13167606ea83a213ab9e10255f09c5389e7910de",
-      startBlock: 42781250,
+      startBlock: 42781255,
     },
     MarketplaceEscrow: {
       chain: "baseSepolia",
       abi: MarketplaceEscrowAbi,
       address: "0x816855Ab573AfE959eBd5a5dc3A263288d194864",
-      startBlock: 42781250,
+      startBlock: 42781255,
     },
   },
 });
